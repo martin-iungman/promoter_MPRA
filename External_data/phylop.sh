4a64061e690915aa597a91de09fd8533b3723ec6
martin <martin.iungman@gmail.com>
2026-03-02 13:10:47 -0300

    phylop update

--- External_data/phylop.sh
+++ External_data/phylop.sh
@@ -1,3 +1,3 @@
-multiBigwigSummary BED-file -b External_data/hg38.phyloP100way.bw -o hg38.phyloP100way_summary_lib.npz --BED Library_data/res/library.bed --outRawCounts External_data/hg38.phyloP100way_summary_lib.bed
+multiBigwigSummary BED-file -b hg38.phyloP100way.bw -o hg38.phyloP100way_summary_lib.npz --BED ../Library_data/res/library.bed --outRawCounts hg38.phyloP100way_summary_lib.bed
 
-multiBigwigSummary BED-file -b External_data/hg38.phyloP100way.bw -o hg38.phyloP100way_summary_lib_1bp.npz --BED Library_data/res/1bp_library.bed --outRawCounts External_data/hg38.phyloP100way_summary_lib_1bp.bed
+multiBigwigSummary BED-file -b hg38.phyloP100way.bw -o hg38.phyloP100way_summary_lib_1bp.npz --BED ../Library_data/res/1bp_library.bed --outRawCounts hg38.phyloP100way_summary_lib_1bp.bed
