f181b117be9d0a3880d301c9d8d1255c2fe6bc01
martin <martin.iungman@gmail.com>
2026-03-02 13:09:09 -0300

    add phylop

---
+++ External_data/phylop.sh
@@ -0,0 +1,3 @@
+multiBigwigSummary BED-file -b External_data/hg38.phyloP100way.bw -o hg38.phyloP100way_summary_lib.npz --BED Library_data/res/library.bed --outRawCounts External_data/hg38.phyloP100way_summary_lib.bed
+
+multiBigwigSummary BED-file -b External_data/hg38.phyloP100way.bw -o hg38.phyloP100way_summary_lib_1bp.npz --BED Library_data/res/1bp_library.bed --outRawCounts External_data/hg38.phyloP100way_summary_lib_1bp.bed
